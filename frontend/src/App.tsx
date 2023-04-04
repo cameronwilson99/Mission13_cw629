c47b55ab20989e025aa5c14036a32bc9f4089372
Cameron Wilson <cameronwilson99@gmail.com>
2023-04-04 09:50:22 -0600

    first api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import MovieList from './MovieList';
+import MovieList from './Movies/MovieList';
 import Home from './Home';
 import Podcast from './Podcast';
 import NavBar from './Layout';
